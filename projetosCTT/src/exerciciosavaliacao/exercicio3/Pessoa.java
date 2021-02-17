deeccc0b1fbaebdbd6015340d44900471eaa3695
Diego Cezarino <cezarino.diego@gmail.com>
2021-02-17 20:52:32 -0300

    Adicionando classe exercício 03

---
+++ projetosCTT/src/exerciciosavaliacao/exercicio3/Pessoa.java
@@ -0,0 +1,15 @@
+package exerciciosavaliacao.exercicio3;
+
+public class Pessoa {
+
+	private String nomePessoa;
+
+	public void setNomePessoa(String nomePessoa) {
+		this.nomePessoa = nomePessoa;
+	}
+
+	public String getNomePessoa() {
+		return nomePessoa;
+	}
+
+}
