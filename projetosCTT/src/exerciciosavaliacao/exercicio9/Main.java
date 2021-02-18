a5775974e6c6527d2d5b792c11d8029741796fc8
Diego Cezarino <cezarino.diego@gmail.com>
2021-02-17 21:10:22 -0300

    Adicionando classe exercício 09

---
+++ projetosCTT/src/exerciciosavaliacao/exercicio9/Main.java
@@ -0,0 +1,15 @@
+package exerciciosavaliacao.exercicio9;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("Minutos para Segundos: " + ConversaoDeUnidadesDeTempo.minutosParaSegunodos(2));
+		System.out.println("Horas para Minutos: " + ConversaoDeUnidadesDeTempo.horasParaMinutos(4));
+		System.out.println("Dias para Horas: " + ConversaoDeUnidadesDeTempo.diasParaHoras(6));
+		System.out.println("Semanaas para Dias: " + ConversaoDeUnidadesDeTempo.semanasParaDias(8));
+		System.out.println("Anos para Dias: " + ConversaoDeUnidadesDeTempo.anosParaDias(10));
+
+	}
+
+}
