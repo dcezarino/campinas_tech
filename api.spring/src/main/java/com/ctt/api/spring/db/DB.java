d3c8f4e753428fa354733732a16f3f73f3687478
Diego Cezarino <cezarino.diego@gmail.com>
2021-02-25 22:08:55 -0300

    Updated DB.java class

---
+++ api.spring/src/main/java/com/ctt/api/spring/db/DB.java
@@ -0,0 +1,15 @@
+package com.ctt.api.spring.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ctt.api.spring.model.Post;
+
+public class DB {
+
+	public static List<Post> posts = new ArrayList<>();
+
+	private DB() {
+	}
+
+}
