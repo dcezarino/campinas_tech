6a9112f9e6ae426bdc02021c854d5ee55dbcafc4
Diego Cezarino <cezarino.diego@gmail.com>
2021-02-25 22:10:39 -0300

    Modified package

--- api.spring/src/main/java/com/ctt/api/spring/controller/PostController.java
+++ api.spring/src/main/java/com/ctt/api/spring/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.ctt.api.spring.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,55 +15,71 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ctt.api.spring.model.Post;
+import com.ctt.api.spring.service.PostService;
 
-//Criação dos métodos controller
 @RestController
 @RequestMapping("/posts")
 public class PostController {
+	
+	private PostService service;
+	
+	@Autowired
+	public PostController(PostService service) {
+		this.service = service;
+	}
 
-	// Inserindo o método GET
-	// @RequestMapping(method = RequestMethod.GET)
 	@GetMapping
-	public List<Post> getAllPosts() {
-		Post post = new Post(1, "Meu primeiro Post", "getAllPosts");
-		return Arrays.asList(post, post, post);
+	public ResponseEntity<List<Post>> getAllPosts() {
+		List<Post> posts = this.service.getAllPosts();
+		
+		if (posts.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.ok().body(posts);
 	}
-
-	// Injetar o valor que está na url nessa variável String id.
-	// @RequestMapping(value = "{id}", method = RequestMethod.GET)
+	
 	@GetMapping("{id}")
-	public Post getPost(@PathVariable("id") Integer id) {
-		return new Post(id, "Meu Post com ID", "getPost");
+	public ResponseEntity<Post> getPost(@PathVariable("id") Integer id) {
+		Post post = this.service.getById(id);
+		
+		if (post == null) {
+			return ResponseEntity.noContent().build(); 
+		}
+		
+		return ResponseEntity.ok(post);
 	}
-
-	// @RequestMapping(method = RequestMethod.POST)
+	
 	@PostMapping
-	public Post createPost(@RequestBody Post post) {
-		int randomID = (int) Math.floor(Math.random() * 10 + 1);
-		post.setId(randomID);
-		return post;
+	public ResponseEntity createPost(@RequestBody Post post) {
+		boolean created = this.service.createPost(post);
+		
+		return created? ResponseEntity.status(HttpStatus.CREATED).build() : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 	}
 
-	// @RequestMapping(value = "{id}", method = RequestMethod.PUT)
 	@PutMapping("{id}")
-	public Post updatePost(@PathVariable("id") Integer id, @RequestBody Post post) {
-		Post postFake = new Post(1, "Meu primeiro Post", "lorem ipsum catipms isso....");
-		Post postARetorar = null;
-		List<Post> posts = Arrays.asList(postFake, postFake, postFake);
-		for (Post post2 : posts) {
-			if (post2.getId() == id) {
-				post2.setDescription(post.getDescription());
-				post2.setTitle(post.getTitle());
-				postARetorar = post2;
-			}
+	public ResponseEntity<Post> updatePost(@PathVariable("id") Integer id, @RequestBody Post postToBeUpdated) {
+	Post post = this.service.getById(id);
+		
+		if (post == null) {
+			return ResponseEntity.noContent().build(); 
 		}
-		return postARetorar;
+		
+		Post postUpdated = this.service.editPost(id, postToBeUpdated);
+		
+		return ResponseEntity.ok(postUpdated);
 	}
+	
 	@DeleteMapping("{id}")
-	public boolean deletePost(@PathVariable("id") Integer id) {
-		if (id == 1) {
-			return true;
+	public ResponseEntity deletePost(@PathVariable("id") Integer id) {
+		Post post = this.service.getById(id);
+		
+		if (post == null) {
+			return ResponseEntity.noContent().build(); 
 		}
-		return false;
+		
+		this.service.delete(id);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).build();
 	}
+	
 }
