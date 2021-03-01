cb07a63758544eba9720965f35c2bfffeb3bfe9a
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-01 15:28:22 -0300

    Updated class

--- lombokteste/src/Main.java
+++ lombokteste/src/Main.java
@@ -2,21 +2,20 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		ClienteModel clienteModel = new ClienteModel();
 		clienteModel.setId(1L);
 		clienteModel.setNome("Diego");
-		
+
 		System.out.println(clienteModel);
-		
+
 		ClienteModel clienteModel2 = new ClienteModel();
 		clienteModel2.setId(1L);
 		clienteModel2.setNome("Carlos Eduardo");
-		
+
 		System.out.println("Comparando objetos com ids iguais: ");
 		System.out.println(clienteModel.equals(clienteModel2));
-		
-		
+
 		ClienteModel clienteModel3 = new ClienteModel();
 		clienteModel3.setId(3L);
 		clienteModel3.setNome("José Maria");
@@ -24,8 +23,8 @@ public class Main {
 		System.out.println("Comparando objetos com ids diferentes");
 		System.out.println(clienteModel.equals(clienteModel3));
 
-				
-
+		CarroModel carroModel = new CarroModel("Amarelo");
+		System.out.println(carroModel);
 
 	}
 
