27a1bd4f7c0674ad29313c0862720f25bae7cd6b
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-01 15:08:30 -0300

    Uploaded new class using Lombok

---
+++ lombokteste/src/Main.java
@@ -0,0 +1,32 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ClienteModel clienteModel = new ClienteModel();
+		clienteModel.setId(1L);
+		clienteModel.setNome("Diego");
+		
+		System.out.println(clienteModel);
+		
+		ClienteModel clienteModel2 = new ClienteModel();
+		clienteModel2.setId(1L);
+		clienteModel2.setNome("Carlos Eduardo");
+		
+		System.out.println("Comparando objetos com ids iguais: ");
+		System.out.println(clienteModel.equals(clienteModel2));
+		
+		
+		ClienteModel clienteModel3 = new ClienteModel();
+		clienteModel3.setId(3L);
+		clienteModel3.setNome("José Maria");
+
+		System.out.println("Comparando objetos com ids diferentes");
+		System.out.println(clienteModel.equals(clienteModel3));
+
+				
+
+
+	}
+
+}
