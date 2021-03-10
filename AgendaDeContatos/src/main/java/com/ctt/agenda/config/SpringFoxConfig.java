77c74f6eb2a4931c2ef12037a55503c283f27eb4
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-10 15:19:53 -0300

    Updated project to work with Swagger APIs RESTful (JSON)

--- AgendaDeContatos/src/main/java/com/ctt/agenda/config/SpringFoxConfig.java
+++ AgendaDeContatos/src/main/java/com/ctt/agenda/config/SpringFoxConfig.java
@@ -14,7 +14,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SpringFoxConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
 	}
 
