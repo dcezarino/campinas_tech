30cd85aabe1cdb2ca2a4ae3eb79d6e1399df1516
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-17 16:11:02 -0300

    Validation was created in the CategoryRequest class

--- ProductPayments/src/main/java/com/ctt/productpayments/entity/Category.java
+++ ProductPayments/src/main/java/com/ctt/productpayments/entity/Category.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
-import javax.validation.constraints.Size;
 
 import com.ctt.productpayments.request.CategoryRequest;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -42,8 +41,7 @@ public class Category {
 	@Column(nullable = false)
 	private String name;
 	
-	@Column(nullable = false)
-	@Size(max = 5)
+	@Column(nullable = false)	
 	private String code;
 	
 	@JsonIgnore
