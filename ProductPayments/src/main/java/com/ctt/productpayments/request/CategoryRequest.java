c0f23583862c5cc55a69f4729a53376fd56fdd06
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-18 16:41:02 -0300

    Removed product list

--- ProductPayments/src/main/java/com/ctt/productpayments/request/CategoryRequest.java
+++ ProductPayments/src/main/java/com/ctt/productpayments/request/CategoryRequest.java
@@ -1,11 +1,7 @@
 package com.ctt.productpayments.request;
 
-import java.util.List;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-
-import com.ctt.productpayments.entity.Product;
 
 import lombok.Data;
 
@@ -18,7 +14,5 @@ public class CategoryRequest {
 	@NotBlank
 	@Size(max = 3, message = "code cannot be greater than three")
 	private String code;
-		
-    private List<Product> products;
-
+	
 }
