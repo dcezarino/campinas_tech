87a9f73d114d706f8272e7be6976bd22347d5318
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-18 16:41:26 -0300

    Updated description to "Not found"

--- ProductPayments/src/main/java/com/ctt/productpayments/exception/NotFound.java
+++ ProductPayments/src/main/java/com/ctt/productpayments/exception/NotFound.java
@@ -3,7 +3,7 @@ package com.ctt.productpayments.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Não encontrado")
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found")
 public class NotFound extends RuntimeException {
 
 	/**
