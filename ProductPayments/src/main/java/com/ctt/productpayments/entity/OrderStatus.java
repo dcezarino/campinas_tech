b5f17dbf7df6275e18c0074924b884668362a679
Diego Cezarino <cezarino.diego@gmail.com>
2021-03-17 21:20:16 -0300

    Updated column to be created with a certain size

--- ProductPayments/src/main/java/com/ctt/productpayments/entity/OrderStatus.java
+++ ProductPayments/src/main/java/com/ctt/productpayments/entity/OrderStatus.java
@@ -37,7 +37,7 @@ public class OrderStatus {
 	@Column(nullable = false)
 	private String name;
 
-	@Column(nullable = false)
+	@Column(nullable = false, length = 5)
 	private String code;
 
 	@Column(nullable = false)
